e26a4c9c6a520ba711eaa0dc92657810f7f9ce63
brettdoyle44@gmail.com <h0ck3ypl@y3r44>
2019-10-14 21:21:04 -0400

    Add username auth
    - Added error handling when username not taken.

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -30,7 +30,7 @@
 </template>
 
 <script>
-import { mapState, mapGetters } from "vuex";
+import { mapState, mapGetters, mapActions } from "vuex";
 
 export default {
   name: "login-form",
@@ -46,6 +46,15 @@ export default {
     },
     ...mapState(["loading", "error"]),
     ...mapGetters(["hasError"])
+  },
+  methods: {
+    ...mapActions(["login"]),
+    async onSubmit() {
+      const result = await this.login(this.userId);
+      if (result) {
+        this.$router.push("chat");
+      }
+    }
   }
 };
 </script>
